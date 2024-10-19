629323c955fb8a2505cccc7dbcf23963fe7d0e6d
tahraouianass <anass.tahraoui98@gmail.com>
2024-10-19 17:15:52 +0100

    Add Models

---
+++ Domain/Models/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Ticket
+    {
+        public int id {  get; set; }
+
+        public string description { get; set; }
+
+        public TicketStatus status { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+
+    public enum TicketStatus
+    {
+        Open,
+        Closed
+    }
+}
