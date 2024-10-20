b82a6445f4c664ac8df1eb585493439a292f9079
tahraouianass <anass.tahraoui98@gmail.com>
2024-10-21 00:55:48 +0100

    Complete Some Function

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b8e5180061d9d6dffb7e565248f4356003ac80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03c0a57f8addd795aa1576bcefac8bfe84060ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
